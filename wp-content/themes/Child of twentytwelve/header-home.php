c2ed2e590379e4a5836bfee4ff34d69a5cb0aa19
Ruiqi Li <liruiqi25@gmail.com>
2018-04-10 17:41:06 -0400

    added google tracking code to header.php

--- wp-content/themes/Child of twentytwelve/header-home.php
+++ wp-content/themes/Child of twentytwelve/header-home.php
@@ -29,6 +29,14 @@
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->
 <?php wp_head(); ?>
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-117319627-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-117319627-1');
+</script>
 </head>
 
 <body <?php body_class(); ?>>
